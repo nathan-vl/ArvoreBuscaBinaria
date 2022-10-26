5f14349ee1fb3e67838a3ef53ba3ce16a98d99a7
Guilherme Euller Dantas Silva <guilhermeeuller10@gmail.com>
2022-10-26 14:41:23 -0300

    feat: implementação do formato 1 de printar arvore

--- src/ArvoreBuscaBinaria.java
+++ src/ArvoreBuscaBinaria.java
@@ -169,24 +169,18 @@ public class ArvoreBuscaBinaria {
 	}
 
 	public boolean ehCompleta(No no) {
-		int max = maxNivel(raiz);
-		
 		if(no.esquerda == null || no.direita == null) {
+			int max = maxNivel(raiz);
 			int nivel = getNivel(no, raiz);
-			if(max == nivel || max - 1 == nivel) {
-				return true;
-			}else{
+			if(!(max == nivel || max - 1 == nivel))
 				return false;
-			}
 		}
 		
-		if (no.esquerda != null) {
+		if (no.esquerda != null)
 			return ehCompleta(no.esquerda);
-		}
-
-		if (no.direita != null) {
+			
+		if (no.direita != null)
 			return ehCompleta(no.direita);
-		}
 		
 		return true;
 	}
@@ -196,12 +190,12 @@ public class ArvoreBuscaBinaria {
 	}
 
 	private int getNivel(No no, No raiz) {
-		if (no.valor > raiz.valor) {
+		if (no.valor > raiz.valor)
 			return 1 + getNivel(no, raiz.direita);
-		}
-		if (no.valor < raiz.valor) {
+
+		if (no.valor < raiz.valor) 
 			return 1 + getNivel(no, raiz.esquerda);
-		}
+		
 		return 1;
 	}
 
@@ -212,13 +206,16 @@ public class ArvoreBuscaBinaria {
 			if(maxAux > max)
 				max = maxAux;
 		}
+		
 		if (no.direita != null) {
 			int maxAux = maxNivel(no.direita);
 			if(maxAux > max)
 				max = maxAux;
 		}
+		
 		if(no.esquerda == null && no.direita == null)
 			max = getNivel(no, raiz);
+		
 		return max;
 	}
 
@@ -240,22 +237,29 @@ public class ArvoreBuscaBinaria {
 	public void imprimeArvore(int s) {
 		if (s == 1 || s == 2) {
 			if (s == 1) {
-				System.out.println(formato1(raiz));
+				formato1(raiz);
 			} else if (s == 2) {
 				System.out.println(formato2(raiz));
 			}
 		}
 	}
 
-	public String formato1(No no) {
-		String retorno = "(" + no.valor;
-		if (no.esquerda != null) {
-			retorno += " " + formato1(no.esquerda);
+	public void formato1(No no) {
+		int nivel = getNivel(no, raiz);
+		for(int i=0; i< nivel - 1; i++) {
+			System.out.print("\t");
+		};
+		System.out.print(no.valor);
+		for(int i=0; i<maxNivel(raiz)-nivel+ 1; i++) {
+			System.out.print("--------");
 		}
-		if (no.direita != null) {
-			retorno += " " + formato1(no.direita);
+		System.out.print("\n");
+		if(no.esquerda != null) {
+			formato1(no.esquerda);
 		}
-		return retorno + ")";
+		if(no.direita != null) {
+			formato1(no.direita);
+		}
 	}
 
 	public String formato2(No no) {
@@ -272,14 +276,12 @@ public class ArvoreBuscaBinaria {
 	public static void main(String[] args) {
 		ArvoreBuscaBinaria arvore = new ArvoreBuscaBinaria(5);
 		arvore.inserir(3);
+		arvore.inserir(2);
+		arvore.inserir(1);
 		arvore.inserir(8);
 		arvore.inserir(6);
 		arvore.inserir(9);
 		
 		arvore.imprimeArvore(1);
-		
-		System.out.println(arvore.maxNivel(arvore.raiz));
-
-		System.out.println(arvore.ehCompleta());
 	}
 }
